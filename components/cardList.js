f9cfa04af78ec096ef8a5999de97505f6e483d68
zz <924797163@qq.com>
2021-03-20 14:14:40 +0800

    fix font size

--- components/cardList.js
+++ components/cardList.js
@@ -41,8 +41,8 @@ export default function CardList({
               <img src={image} alt="" />
             </div>
             <span className={`w-bk2wl text-h4 pl-w1 pr-w1 mt-h5 z-30 transition-opacity duration-500 ${!isTransitionEnd && 'opacity-0'}`}>{title}</span>
-            <div className={`flex flex-row mb-h2 w-bk2wl justify-between pr-w1 z-30 transition-opacity duration-500 ${!isTransitionEnd && 'opacity-0'}`}>
-              <span className="pl-w1 text-right pr-w1">{tags}</span>
+            <div className={`flex flex-col mb-h2 w-bk2wl justify-between pr-w1 z-30 transition-opacity duration-500 ${!isTransitionEnd && 'opacity-0'}`}>
+              <span className="pl-w1 text-right pb-h1">{tags}</span>
               <div className="flex flex-col text-gray-400">
                 <span className="text-right text-content">{date.split(' ')[1]}</span>
                 <span className="text-right text-content">{date.split(' ')[0]}</span>
